9db54317136376065806345a7b57d2970df90630
Suraj <87514ss@gmail.com>
2022-01-28 21:56:13 +0530

    change width and height of screen

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ class Settings() :
     """initialise the game settings """
     def __init__(self):
         #screen settings
-        self.height = 700
-        self.width = 700
+        self.height = 650
+        self.width = 750
         self.bg_color = (150,0,180)
 
